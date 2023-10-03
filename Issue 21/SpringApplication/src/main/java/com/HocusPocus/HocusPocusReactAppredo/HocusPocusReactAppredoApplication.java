11267aaab7b3576fa989e250f3e8820c2c587a6b
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2023-10-03 13:19:28 -0600

    6th try

---
+++ Issue 21/SpringApplication/src/main/java/com/HocusPocus/HocusPocusReactAppredo/HocusPocusReactAppredoApplication.java
@@ -0,0 +1,13 @@
+package com.HocusPocus.HocusPocusReactAppredo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocusPocusReactAppredoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocusPocusReactAppredoApplication.class, args);
+	}
+
+}
