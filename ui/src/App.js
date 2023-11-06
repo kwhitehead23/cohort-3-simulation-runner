28859818b4b12b7b768c5821ab4fe9b73926e018
calisward <calisward2005@gmail.com>
2023-11-06 15:39:56 -0600

    settings ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-import Grid from "./Components/Grid";
+
+import Grid from "./Components/grid";
+import Settings from "./Components/settings";
+
 function App() {
   return (
     <>
-   
+      <div className="settings">
+        <Settings />
+      </div>
+
       <Grid />
     </>
   );
