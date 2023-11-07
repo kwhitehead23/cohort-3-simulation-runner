766a623764b9e7ad18a7939bffa6e80a68773b0e
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2023-11-07 16:18:52 -0600

    Making it pretty cool

--- ui/src/pages/Simulation.js
+++ ui/src/pages/Simulation.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Grid from "../Components/grid";
 import Legend from "../Components/Legend";
-import mazes from "../Data/mazes.json";
+import Layouts from "../Data/Layouts.json";
 
 function Simulation(g) {
   return (
     <div>
       <h1>Simulation Page</h1>
       <Legend />
-      <Grid data={mazes} />
+      <Grid data={Layouts} />
     </div>
   );
 }
