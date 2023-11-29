731e5913a90e307df91e45bf96aea972c6c6c8c0
GabrielIvey0 <ivey2252@gmail.com>
2023-11-29 11:27:41 -0600

    added rule set code and web flux dependency

---
+++ src/main/java/com/HocusPocus/Simulation/simulationDb/models/RuleSet.java
@@ -0,0 +1,9 @@
+package com.HocusPocus.Simulation.simulationDb.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RuleSet {
+    public Long id;
+    public String name;
+    public String creationDate;
+}
