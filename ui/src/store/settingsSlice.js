e2d3619ef44cf13f84c9aa1b88e575b2f793d3b0
Christopher Marfia <chris.marfia@daugherty.com>
2023-11-30 06:49:31 -0600

    Add redux and the settings slice

---
+++ ui/src/store/settingsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const settingsSlice = createSlice({
+  name: 'settings',
+  initialState: {
+    layoutId: null,
+    rulesetId: null,
+    maxIterations: 100
+  },
+  reducers: {
+    updateLayout: (state, action) => {
+        state.layoutId = action.payload;
+    },
+    updateRuleset: (state, action) => {
+        state.rulesetId = action.payload;
+    },
+    updateMaxIterations: (state, action) => {
+        state.maxIterations = Number(action.payload, 10);
+    }
+  },
+})
+
+export const { updateLayout, updateRuleset, updateMaxIterations } = settingsSlice.actions
+
+export default settingsSlice.reducer
