1df40ca8d965992c7f34098775d18d0f7b4e3144
Christopher Marfia <chris.marfia@daugherty.com>
2023-11-30 06:48:43 -0600

    Fix the mocks that were created with extra pieces

--- mocks/layouts.mock.js
+++ mocks/layouts.mock.js
@@ -1,20 +1,8 @@
 let mockLayouts = require("./data/layouts.json");
 
 const proxy = {
-  _proxy: {
-    changeHost: true,
-    httpProxy: {
-      options: {
-        ignorePath: true,
-      },
-    },
-  },
   "GET /layouts": (_, res) => {
     res.json(mockLayouts);
-  },
-  "DELETE /layouts/:id": (req, res) => {
-    mockLayouts = mockLayouts.filter(({ id }) => id != req.params.id);
-    res.send();
   },
 };
 module.exports = proxy;
